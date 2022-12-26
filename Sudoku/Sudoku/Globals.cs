5c5d3bdc4fce5bba370bc81b72f5323c2632d55f
omerzifman <67154440+omerzifman@users.noreply.github.com>
2022-12-26 13:19:55 +0200

    Create Globals.cs
    global variables

---
+++ Sudoku/Sudoku/Globals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku
+{
+    public static class Globals
+    {
+        public static int[][] board;
+        public const int rows = 8; //9, arr index is 0-8
+        public const int cols = 8; //9, arr index is 0-8
+    }
+}
