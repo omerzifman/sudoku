27c27e1bd131c35e683d651f4d8dc2579d81735b
omerzifman <67154440+omerzifman@users.noreply.github.com>
2022-12-26 13:38:12 +0200

    Update Globals.cs
    syntax error fixed

--- Sudoku/Sudoku/Globals.cs
+++ Sudoku/Sudoku/Globals.cs
@@ -6,8 +6,8 @@ namespace Sudoku
 {
     public static class Globals
     {
-        public static int[][] board;
         public const int rows = 8; //9, arr index is 0-8
         public const int cols = 8; //9, arr index is 0-8
+        public static int[,] board = new int[rows, cols];
     }
 }
