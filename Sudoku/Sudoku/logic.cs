d6892d7eaf793fa87420a0b19b4968db4b025141
omerzifman <67154440+omerzifman@users.noreply.github.com>
2023-01-02 12:02:26 +0200

    Update logic.cs
    faster solving func - solveBoard2.
    in isInBox() func, boxSize updated by grid size for n*n grids

--- Sudoku/Sudoku/logic.cs
+++ Sudoku/Sudoku/logic.cs
@@ -33,12 +33,13 @@ namespace Sudoku
         private static bool isInBox(int[,] board, int number, int row, int col)
         { // returns true if num is in a given box in board, else false.
             //the box is the one the [row,col] is in
-            int BoxRow = row - row % 3; //calc box starting index - row
-            int BoxCol = col - col % 3;//calc box starting index - col
+            int BoxSize = (int)Math.Sqrt(Globals.rows); //box row/col size
+            int BoxRow = row - row % BoxSize; //calc box starting index - row
+            int BoxCol = col - col % BoxSize;//calc box starting index - col
 
-            for (int i = BoxRow; i < BoxRow + 3; i++)
+            for (int i = BoxRow; i < BoxRow + BoxSize; i++)
             {
-                for (int j = BoxCol; j < BoxCol + 3; j++)
+                for (int j = BoxCol; j < BoxCol + BoxSize; j++)
                 {
                     if (board[i,j] == number)
                     {
@@ -87,9 +88,30 @@ namespace Sudoku
             return true;
         }
 
+        private static bool solveBoard2(int[,] board, int row = 0, int col = 0)
+        { // solves the board and changes the given board. returns true if succeeded, else false
+            if (row == Globals.rows)
+                return true;
+            else if (col == Globals.cols)
+                return solveBoard2(board,row+1,0);
+            else if(board[row, col] != 0)
+                return solveBoard2(board, row , col+1);
+            for(int number = 1; number <= Globals.rows; number++)
+            {
+                if(isValidPlacement(board, number, row, col))
+                {
+                    board[row, col] = number;
+                    if (solveBoard2(board, row, col + 1))
+                        return true;
+                    board[row, col] = 0;
+                }
+            }
+            return false;
+        }
+
         public static int[,] solve()
         { //returns the solved board. can throw UnsolvableBoardException
-            if (solveBoard(Globals.board))
+            if (solveBoard2(Globals.board))
                 return Globals.board;
             else
             {
