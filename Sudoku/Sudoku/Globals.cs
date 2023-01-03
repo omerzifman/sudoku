b6953ecf812ecb19fb3f0692c7507a1c76f3d39f
omerzifman <67154440+omerzifman@users.noreply.github.com>
2023-01-03 13:56:13 +0200

    Update Globals.cs
    now saves the board in cell[,]. the cell has int value, int[] possibilities, bool given.
    global board updated

--- Sudoku/Sudoku/Globals.cs
+++ Sudoku/Sudoku/Globals.cs
@@ -4,17 +4,31 @@ using System.Text;
 
 namespace Sudoku
 {
+    public class cell
+    {
+        public int value //value at cell
+        { get; set; }
+        public int[] possibilities; //possible values for cell
+        public bool given //true if the cell is given in the original board
+        { get; set; }
+        public cell(int size, int value, bool given)
+        { // init the cell
+            this.value = value;
+            possibilities = new int[size];
+            this.given = given;
+        }
+    }
     public static class Globals
     {
         public static int rows; //board index is 0-size
         public static int cols; //board index is 0-size
-        public static int[,] board; //sudoku board
+        public static cell[,] board; //sudoku board
 
         public static void InitGlobals(int size)
         { //init size*size board
             rows = size;
             cols = size;
-            board = new int[rows, cols]; //init sudoku board for size*size
+            board = new cell[rows, cols]; //init sudoku board for size*size
         }
     }
 }
