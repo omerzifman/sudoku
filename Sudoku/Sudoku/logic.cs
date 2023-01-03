06cd2f430958c3df51b60ff0c90fda17472a5e4b
omerzifman <67154440+omerzifman@users.noreply.github.com>
2023-01-03 13:58:16 +0200

    logic updated
    logic updated to support cell[,] boards. use solveBoard3 to solve board faster - uses new func build_possibles

--- Sudoku/Sudoku/logic.cs
+++ Sudoku/Sudoku/logic.cs
@@ -6,11 +6,11 @@ namespace Sudoku
 {
     public class logic
     {
-        private static bool isInRow(int[,] board, int number, int row)
+        private static bool isInRow(cell[,] board, int number, int row)
         { // returns true if num is in a given row in board, else false.
             for (int i = 0; i < Globals.rows; i++)
             {
-                if (board[row,i] == number)
+                if (board[row,i].value == number)
                 {
                     return true;
                 }
@@ -18,11 +18,11 @@ namespace Sudoku
             return false;
         }
 
-        private static bool isInCol(int[,] board, int number, int col)
+        private static bool isInCol(cell[,] board, int number, int col)
         { // returns true if num is in a given col in board, else false.
             for (int i = 0; i < Globals.cols; i++)
             {
-                if (board[i,col] == number)
+                if (board[i,col].value == number)
                 {
                     return true;
                 }
@@ -30,7 +30,7 @@ namespace Sudoku
             return false;
         }
 
-        private static bool isInBox(int[,] board, int number, int row, int col)
+        private static bool isInBox(cell[,] board, int number, int row, int col)
         { // returns true if num is in a given box in board, else false.
             //the box is the one the [row,col] is in
             int BoxSize = (int)Math.Sqrt(Globals.rows); //box row/col size
@@ -41,7 +41,7 @@ namespace Sudoku
             {
                 for (int j = BoxCol; j < BoxCol + BoxSize; j++)
                 {
-                    if (board[i,j] == number)
+                    if (board[i,j].value == number)
                     {
                         return true;
                     }
@@ -50,7 +50,7 @@ namespace Sudoku
             return false;
         }
 
-        private static bool isValidPlacement(int[,] board, int number, int row, int col)
+        private static bool isValidPlacement(cell[,] board, int number, int row, int col)
         { //returns true if a num can be placed in a [row,col]
             return !isInRow(board, number, row) &&
                 !isInCol(board, number, col) &&
@@ -67,7 +67,7 @@ namespace Sudoku
                     {
                         for (int numberToTry = 1; numberToTry <= Globals.rows; numberToTry++)
                         {
-                            if (isValidPlacement(board, numberToTry, row, col))
+                            if (true)//isValidPlacement(board, numberToTry, row, col))
                             {
                                 board[row,col] = numberToTry;
 
@@ -98,7 +98,7 @@ namespace Sudoku
                 return solveBoard2(board, row , col+1);
             for(int number = 1; number <= Globals.rows; number++)
             {
-                if(isValidPlacement(board, number, row, col))
+                if(true)//isValidPlacement(board, number, row, col))
                 {
                     board[row, col] = number;
                     if (solveBoard2(board, row, col + 1))
@@ -108,11 +108,68 @@ namespace Sudoku
             }
             return false;
         }
+        private static cell[,] build_possibles(cell[,] board)
+        {
+            for (int row = 0; row < Globals.rows; row++)
+            {
+                for (int col = 0; col < Globals.cols; col++)
+                {
+                    if (!board[row, col].given)
+                    {
+                        for (int number = 1; number <= Globals.rows; number++)
+                        {
+                            if (isValidPlacement(board, number, row, col))
+                            {
+                                board[row, col].possibilities[number - 1] = number;
+                            }
+                            else
+                                board[row, col].possibilities[number - 1] = 0;
+                        }
+                    }
+                }
+            }
+            return board;
+        }
+        private static bool solveBoard3(cell[,] board, int row = 0, int col = 0)
+        { // solves the board and changes the given board. returns true if succeeded, else false
+            if (row == Globals.rows)
+                return true;
+            else if (col == Globals.cols)
+                return solveBoard3(board, row + 1, 0);
+            else if (board[row, col].given)
+                return solveBoard3(board, row, col + 1);
+            for (int index = 0; index < Globals.rows; index++)
+            {
+                int number = board[row, col].possibilities[index];
+                if (number != 0)
+                {
+                    if (isValidPlacement(board, number, row, col))
+                    {
+                        board[row, col].value = number;
+                        if (solveBoard3(board, row, col + 1))
+                            return true;
+                    }
+                    board[row, col].value = 0;
+                }
+            }
+            return false;
+        }
 
-        public static int[,] solve()
-        { //returns the solved board. can throw UnsolvableBoardException
-            if (solveBoard2(Globals.board))
-                return Globals.board;
+        public static string solve()
+        {
+            if (solveBoard3(build_possibles(Globals.board)))
+            {
+                string boardStr = "";
+                for (int row = 0; row < Globals.rows; row++)
+                {
+                    for (int col = 0; col < Globals.cols; col++)
+                    {
+                        boardStr = boardStr + (char)(Globals.board[row, col].value + '0');
+                    }
+                }
+                return boardStr;
+            }
+
             else
             {
                 UnsolvableBoardException unsolvableBoard = new UnsolvableBoardException();
